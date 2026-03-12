979df6a6b866fe1e355350027449317a338acd2d
Vimlesh <vimleshkumarchuahan74.8@gmail.com>
2026-03-12 11:16:22 +0530

    made and understands various routes like get,put,post

--- src/app.js
+++ src/app.js
@@ -2,24 +2,42 @@ const express = require("express")
 
 const app = express();
 
+
+//-------------------------------- app.use routes ---------------------------
 // app.get("/",(req,res)=>{
 //     res.send("Inside the get request");
 // })
 
+
+
+// app.use("/test",(req,res)=>{
+//     res.send("Inside the test page");
+// });
+
+// app.use("/test/hello",(req,res)=>{
+//     res.send("ji namste kaise ho");
+// });
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello from Vimlesh");
+// });
+
+
+//------------------------------- app.use routes ----------------------------
+
+
 app.get("/",(req,res)=>{
-    res.send("Inside the get request");
+    res.send("Api using get !");
 });
 
-app.get("/test",(req,res)=>{
-    res.send("Inside the test page");
+app.post("/test",(req,res)=>{
+    res.send("api using the post request");
 });
 
-app.get("/test/hello",(req,res)=>{
-    res.send("ji namste kaise ho");
+app.put("/hello",(req,res)=>{
+    res.send("Working on the put request");
 });
-
 
 app.listen(3000,()=>{
     console.log("Server is started");
-}
-);
+});
