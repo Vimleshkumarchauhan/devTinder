b28e24b6eaa5bade073da407285286c6208c0e5f
Vimlesh <vimleshkumarchuahan74.8@gmail.com>
2026-03-22 09:15:26 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -24,7 +24,37 @@ app.post("/signup",async (req,res)=>{
     }
 
     
+});
+
+app.get("/user",async (req,res)=>{
+
+    const userEmailId = req.body.emailId;
+    
+    try{
+        const user = await User.find({emailId:userEmailId});
+        if (user.length === 0){
+            res.status(404).send("User not found");
+        }else{
+            res.send(user);
+        }
+        
+    }catch{
+        res.status(400).send("something went wrong");
+    }
+});
+
+
+//Feed API - Feed/get the data from database.
+
+app.get("/feed",async (req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch{
+        res.status(400).send("Something went wrong");
+    }
 })
+
 
 connectDB().then(()=>{
     console.log("Database connected successfully");
